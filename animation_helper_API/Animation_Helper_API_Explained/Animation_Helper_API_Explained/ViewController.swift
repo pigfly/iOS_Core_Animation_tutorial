67c9bf8ef59ee5dcd3e1340cc78c77ad50925177
Alex Jiang <alex.jiang@outware.com.au>
2016-04-01 18:01:48 +1100

    first commit for playing with UIDynamics

--- animation_helper_API/Animation_Helper_API_Explained/Animation_Helper_API_Explained/ViewController.swift
+++ animation_helper_API/Animation_Helper_API_Explained/Animation_Helper_API_Explained/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var square: UIView!
+    var barrier: UIView!
+    
+    var dynamicAnimator = UIDynamicAnimator?()
+    
+    var gravityBehavior = UIGravityBehavior!()
+    var collisionBehavior = UICollisionBehavior!()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // square init
+        square = UIView.init(frame: CGRectMake(100, 100, 100, 100))
+        square?.backgroundColor = UIColor.greenColor()
+        view.addSubview(square)
+        
+        // barrier init
+        barrier = UIView.init(frame: CGRectMake(0, 300, 130, 20))
+        barrier.backgroundColor = UIColor.grayColor()
+        view.addSubview(barrier)
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: view)
+        
+        gravityBehavior = UIGravityBehavior.init(items: [square])
+        collisionBehavior = UICollisionBehavior.init(items: [square])
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        
+        // dynamics only affect views that have been associated with behaviors.
+        dynamicAnimator?.addBehavior(gravityBehavior)
+        dynamicAnimator?.addBehavior(collisionBehavior)
+        
+        // setup boundary for barrier
+        setUpBoundaryForView(barrier)
+    }
     
+    
+    /**
+     adds an invisible boundary that coincides with the top edge of the barrier view.
+     
+     - parameter view: view to add boundary
+     */
+    func setUpBoundaryForView(view: UIView) {
+        let rightEdge = CGPointMake(view.frame.origin.x + view.frame.size.width, view.frame.origin.y)
+        
+        collisionBehavior.addBoundaryWithIdentifier("barrier", fromPoint: barrier.frame.origin, toPoint: rightEdge)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    
 }
 
